965fb4e21f493ca16483946127161077fb0f3cc7
Jim Pivarski <jpivarski@gmail.com>
2016-02-03 15:54:24 -0600

    access libm.so to demonstrate JNA

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -4,9 +4,24 @@ import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.Matchers
 
+import com.sun.jna._
+
 class DefaultSuite extends FlatSpec with Matchers {
-  "Nothing" should "do anything" in {
-    println("wowie")
+  "Experiments" should "do something, maybe" in {
+    trait LibM extends Library {
+      def sin(d: Double): Double
+    }
+    val libm = Native.loadLibrary("m", classOf[LibM]).asInstanceOf[LibM]
+
+    object LibM2 {
+      Native.register("m")
+
+      @native
+      def sin(d: Double): Double
+    }
+
+    println(s"sin(0.5) is ${libm.sin(0.5)} vs ${LibM2.sin(0.5)} vs ${Math.sin(0.5)}")
+
   }
 
 }

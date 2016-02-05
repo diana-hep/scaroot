f130f3cd10c90143aa0b9ccf5686fae3fb1d4931
Jim Pivarski <jpivarski@gmail.com>
2016-02-05 09:36:23 -0600

    Here's an example that completely works.

--- src/main/cpp/scaroot.cpp
+++ src/main/cpp/scaroot.cpp
@@ -1,5 +1,26 @@
-#include <stdio.h>
+// g++ -fPIC -shared -Wl,--no-as-needed `root-config --cflags --ldflags --libs` -o target/classes/scaroot.so src/main/cpp/scaroot.cpp
 
-int getEntries(char *fileName, char *treeName) {
-  return 123;
+#include <stdint.h>
+
+#include "TFile.h"
+
+extern "C" {
+  int64_t new_TFile(char *fileName);
+  void delete_TFile(int64_t pointer);
+  void TFile_ls(int64_t pointer);
+}
+
+int64_t new_TFile(char *fileName) {
+  TFile *tfile = new TFile(fileName);
+  return (int64_t)tfile;
+}
+
+void delete_TFile(int64_t pointer) {
+  TFile *tfile = (TFile*)pointer;
+  delete tfile;
+}
+
+void TFile_ls(int64_t pointer) {
+  TFile *tfile = (TFile*)pointer;
+  tfile->ls();
 }
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -7,55 +7,20 @@ import org.scalatest.Matchers
 import com.sun.jna._
 
 class DefaultSuite extends FlatSpec with Matchers {
-  "libm" should "do calculate sin(0.5), without and with native optimization" in {
-    trait LibM extends Library {
-      def sin(d: Double): Double
-    }
-    val libm = Native.loadLibrary("m", classOf[LibM]).asInstanceOf[LibM]
-
-    object LibM2 {
-      Native.register("m")
-
-      @native
-      def sin(d: Double): Double
+  "Shared object" should "load and show some TFile stuff" in {
+    object SharedObject extends Library {
+      Native.register("target/classes/scaroot.so")
+      @native def new_TFile(fileName: String): Long
+      @native def delete_TFile(pointer: Long): Unit
+      @native def TFile_ls(pointer: Long): Unit
     }
 
-    println(s"sin(0.5) is ${libm.sin(0.5)} vs ${LibM2.sin(0.5)} vs ${Math.sin(0.5)}")
-  }
-
-  "custom shared object" should "do something, anything" in {
-    trait LibScaROOT extends Library {
-      def _Z10getEntriesPcS_(fileName: String, treeName: String): Int
-    }
-    val libScaROOT = Native.loadLibrary("scaroot.so", classOf[LibScaROOT]).asInstanceOf[LibScaROOT]
-
-    println(s"""hey ${libScaROOT._Z10getEntriesPcS_("one", "two")}""")
-
-  }
-
-  it should "not crash" in {
-    object LibJaROOT extends Library {
-      Native.register("target/classes/jaroot.so")
-      @native def Java_org_dianahep_jaroot_TFileWrapper_new_1TFile(fileName: String): Long
-      @native def Java_org_dianahep_jaroot_TFileWrapper_delete_1TFile(pointer: Long): Unit
-      @native def Java_org_dianahep_jaroot_TFileWrapper_TFile_1ls(pointer: Long): Unit
-    }
-
-    val pointer = LibJaROOT.Java_org_dianahep_jaroot_TFileWrapper_new_1TFile("/opt/root/test/Event.root")
-    //  *** Break *** segmentation violation
-    //  Generating stack trace...
-    // /usr/bin/addr2line: '/tmp/jna--203800637/jna1322466507952658425.tmp': No such file
-    // /usr/bin/addr2line: '/tmp/jna--203800637/jna1322466507952658425.tmp': No such file
-    // /usr/bin/addr2line: '/tmp/jna--203800637/jna1322466507952658425.tmp': No such file
-    // /usr/bin/addr2line: '/tmp/jna--203800637/jna1322466507952658425.tmp': No such file
-    //  0x00007ff464d3265c in ffi_call_unix64 + 0x4c from /tmp/jna--203800637/jna1322466507952658425.tmp
-    //  0x00007ff464d32164 in ffi_call + 0x1d4 from /tmp/jna--203800637/jna1322466507952658425.tmp
-    //  0x00007ff464d27489 in <unknown> from /tmp/jna--203800637/jna1322466507952658425.tmp
-    //  0x00007ff464d32398 in ffi_closure_unix64_inner + 0x88 from /tmp/jna--203800637/jna1322466507952658425.tmp
-    //  0x00007ff464d327c4 in ffi_closure_unix64 + 0x46 from /tmp/jna--203800637/jna1322466507952658425.tmp
-    //  0x00007ff46d0153f4 in <unknown function>
-
-
+    val pointer = SharedObject.new_TFile("/opt/root/test/Event.root")
+    println(s"pointer value $pointer")
+    SharedObject.TFile_ls(pointer)
+    println(s"see a listing?")
+    SharedObject.delete_TFile(pointer)
+    println(s"still here?")
 
   }
 }

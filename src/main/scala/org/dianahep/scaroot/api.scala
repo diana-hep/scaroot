8545f10ce4d6735905ee7db4374f23b250db1447
Jim Pivarski <jpivarski@gmail.com>
2016-02-09 17:49:29 -0600

    working out structure

--- src/main/scala/org/dianahep/scaroot/api.scala
+++ src/main/scala/org/dianahep/scaroot/api.scala
@@ -3,9 +3,15 @@ package org.dianahep.scaroot
 import scala.language.experimental.macros 
 import scala.reflect.macros.blackbox.Context
 
+package object api {
+  type Identifier = Long   // may be a pointer on the backend
+}
+
 package api {
   trait RootTTreeRowBuilder[T] {
     def build(rootTTree: RootTTreeReader[T], row: Int): T
+    def leafIdentifiers: Array[Identifier]
+    def nameTypes: Seq[(String, String)]
   }
   object RootTTreeRowBuilder {
     implicit def compileRootTTreeRowBuilder[T]: RootTTreeRowBuilder[T] = macro compileRootTTreeRowBuilderImpl[T]
@@ -18,27 +24,32 @@ package api {
         case m: MethodSymbol if (m.isPrimaryConstructor) => m
       }.get.paramLists.head
 
-      val params = fields.map {field =>
+      val (buildParams, nameTypes) = fields.zipWithIndex.map {case (field, index) =>
         val name = field.asTerm.name
         val leafName = name.decodedName.toString
-        val returnType = tpe.decl(name).typeSignature
+        val NullaryMethodType(leafType) = tpe.decl(name).typeSignature
+        // scala.reflect.runtime.universe.Type???
 
-        q"rootTTree.getLeafDValue($leafName, row)"
-      }
+        (q"rootTTree.getLeafDValue(leafIdentifiers($index), row)", q"$leafName -> ${leafType.toString}")
+      }.unzip
 
       c.Expr[RootTTreeRowBuilder[T]](q"""
         new RootTTreeRowBuilder[$tpe] {
-          def build(rootTTree: RootTTreeReader[$tpe], row: Int): $tpe = new $tpe(..$params)
+          def build(rootTTree: RootTTreeReader[$tpe], row: Int): $tpe = new $tpe(..$buildParams)
+          val leafIdentifiers = Array.fill(${fields.size})(0L)
+          val nameTypes = Vector(..$nameTypes)
         }
       """)
     }
   }
 
   class RootTTreeReader[T](val rootFileLocation: String, val ttreeLocation: String, rowBuilder: RootTTreeRowBuilder[T]) {
-    def getLeafDValue(leafName: String, row: Int): Double = 3.14
-    def getLeafCValue(leafName: String, row: Int): String = "hello"
+    println(rowBuilder.nameTypes)
 
-    // TLeafB, TLeafC, TLeafD, TLeafElement, TLeafF, TLeafI, TLeafObject, TLeafS
+    def getLeafDValue(leaf: Identifier, row: Int): Double = 3.14
+    def getLeafCValue(leaf: Identifier, row: Int): String = "hello"
+
+    // TLeafB, TLeafC, TLeafD, TLeafF, TLeafI, TLeafS
 
     def get(row: Int): T = rowBuilder.build(this, row)
   }

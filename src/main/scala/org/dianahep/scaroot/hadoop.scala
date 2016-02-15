9fe3eb2c7b82c2f6f1e18470b58b6965da9f7a56
Jim Pivarski <jpivarski@gmail.com>
2016-02-15 17:21:06 -0600

    RootInputFormat compiles

--- src/main/scala/org/dianahep/scaroot/hadoop.scala
+++ src/main/scala/org/dianahep/scaroot/hadoop.scala
@@ -20,73 +20,72 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext
 
 import org.dianahep.scaroot.freehep.FreeHepRootTTreeReader
 import org.dianahep.scaroot.native.NativeRootTTreeReader
+import org.dianahep.scaroot.api.RootTTreeRowBuilder
 
 package hadoop {  
-  trait RootInputFormat[CASE] extends FileInputFormat[LongWritable, CASE] {
-    def ttreeLocation: String
-
+  abstract class RootInputFormat[CASE: RootTTreeRowBuilder](ttreeLocation: String) extends FileInputFormat[LongWritable, CASE] {
     override def createRecordReader(split: InputSplit, context: TaskAttemptContext): RecordReader[LongWritable, CASE] =
-      new RootRecordReader[CASE](ttreeLocation)
+      new RootRecordReader
 
     override def isSplitable(context: JobContext, file: Path): Boolean = false
 
     override def getSplits(job: JobContext): java.util.List[InputSplit] =
       super.getSplits(job)  // does the right thing; this is here as a reminder that it's overridable
-  }
 
-  object RootInputFormat {
-    def apply[CASE](ttreeLocation: String): RootInputFormat[CASE] = macro applyImpl[CASE]
+    class RootRecordReader extends RecordReader[LongWritable, CASE] {
+      private var reader: FreeHepRootTTreeReader[CASE] = null
+      private var row = -1L
+      private var key: LongWritable = null
+      private var value: CASE = null.asInstanceOf[CASE]
 
-    def applyImpl[CASE : c.WeakTypeTag](c: Context)(ttreeLocation: c.Expr[String]): c.Expr[RootInputFormat[CASE]] = {
-      import c.universe._
-      val caseType = weakTypeOf[CASE]
+      override def initialize(split: InputSplit, context: TaskAttemptContext) = split match {
+        case fileSplit: FileSplit =>
+          val job = context.getConfiguration
+          val fileSystem = FileSystem.get(job)
+          val localFileSystem = FileSystem.getLocal(job)
 
-      c.Expr[RootInputFormat[CASE]](q"""
-        import org.dianahep.scaroot.hadoop._
-        new RootInputFormat[$caseType] {
-          val ttreeLocation = $ttreeLocation
-        }
-      """)
-    }
-  }
+          // Copy file from HDFS to local file system (verified temporary for successful and unsuccessful jobs).
+          // This feels wrong, but I don't see a way around it.
+          val name = fileSplit.getPath.getName
+          fileSystem.copyToLocalFile(false, fileSplit.getPath, localFileSystem.getWorkingDirectory)
 
-  class RootRecordReader[CASE](ttreeLocation: String) extends RecordReader[LongWritable, CASE] {
-    private var reader: FreeHepRootTTreeReader[CASE] = null
-    private var row = -1L
-    private var key: LongWritable = null
-    private var value: CASE = null.asInstanceOf[CASE]
-
-    override def initialize(split: InputSplit, context: TaskAttemptContext) = split match {
-      case fileSplit: FileSplit =>
-        val job = context.getConfiguration
-        val fileSystem = FileSystem.get(job)
-        val localFileSystem = FileSystem.getLocal(job)
-
-        // Copy file from HDFS to local file system (verified temporary for successful and unsuccessful jobs).
-        // This feels wrong, but I don't see a way around it.
-        val name = fileSplit.getPath.getName
-        fileSystem.copyToLocalFile(false, fileSplit.getPath, localFileSystem.getWorkingDirectory)
-
-        val file = localFileSystem.pathToFile(new Path(localFileSystem.getWorkingDirectory, name))
-        reader = FreeHepRootTTreeReader[CASE](file.getAbsolutePath, ttreeLocation)
-    }
-
-    override def nextKeyValue(): Boolean = {
-      row += 1L
-      if (row < reader.size) {
-        key = new LongWritable(row)
-        value = reader.get(row)
-        true
+          val file = localFileSystem.pathToFile(new Path(localFileSystem.getWorkingDirectory, name))
+          reader = new FreeHepRootTTreeReader[CASE](file.getAbsolutePath, ttreeLocation, implicitly[RootTTreeRowBuilder[CASE]])
       }
-      else
-        false
-    }
-    override def getCurrentKey() = key
-    override def getCurrentValue() = value
-    override def getProgress() = row.toFloat / reader.size.toFloat
-    override def close() {
-      reader.release()
+
+      override def nextKeyValue(): Boolean = {
+        row += 1L
+        if (row < reader.size) {
+          key = new LongWritable(row)
+          value = reader.get(row)
+          true
+        }
+        else
+          false
+      }
+      override def getCurrentKey() = key
+      override def getCurrentValue() = value
+      override def getProgress() = row.toFloat / reader.size.toFloat
+      override def close() {
+        reader.release()
+      }
     }
   }
+
+  // object RootInputFormat {
+  //   def apply[CASE](ttreeLocation: String): RootInputFormat[CASE] = macro applyImpl[CASE]
+
+  //   def applyImpl[CASE : c.WeakTypeTag](c: Context)(ttreeLocation: c.Expr[String]): c.Expr[RootInputFormat[CASE]] = {
+  //     import c.universe._
+  //     val caseType = weakTypeOf[CASE]
+
+  //     c.Expr[RootInputFormat[CASE]](q"""
+  //       import org.dianahep.scaroot.hadoop._
+  //       new RootInputFormat[$caseType] {
+  //         val ttreeLocation = $ttreeLocation
+  //       }
+  //     """)
+  //   }
+  // }
 
 }

bf15ff9a31efb0144f6f37a27666429d0cde3200
Jim Pivarski <jpivarski@gmail.com>
2016-02-17 18:36:11 +0000

    Wrapped up nicely, but doesn't work because Spark assumes that the InputFormat has a zero-argument constructor.

--- src/main/scala/org/dianahep/scaroot/spark.scala
+++ src/main/scala/org/dianahep/scaroot/spark.scala
@@ -1,11 +1,22 @@
 package org.dianahep.scaroot
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import org.dianahep.scaroot.api._
 import org.dianahep.scaroot.hadoop._
 
-package spark {
+package object spark {
+  implicit class SparkContextWrapper(sc: SparkContext) {
+    def rootRDD[T : RootTTreeRowBuilder : ValueSerializer](path: String, ttreeLocation: String): RDD[T] = {
+      class SpecificWritable extends ValueWritable[T]
+      class SpecificInputFormat extends RootInputFormat[T, SpecificWritable](ttreeLocation)
+      sc.newAPIHadoopFile[KeyWritable, SpecificWritable, SpecificInputFormat](path) map {case (_, ValueWritable(x)) => x}
+    }
 
-
-
-
-
+    def quicktest[T : RootTTreeRowBuilder : ValueSerializer]: RDD[T] = {
+      rootRDD[T]("/home/ubuntu/TrackResonanceNtuple.root", "TrackResonanceNtuple/twoMuon")
+      // sc.newAPIHadoopFile[KeyWritable, SpecificWritable2, SpecificInputFormat2]("/home/ubuntu/TrackResonanceNtuple.root")
+    }
+  }
 }

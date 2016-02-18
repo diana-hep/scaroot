e8126b828862595855fd6558efc25eaba530bc78
Jim Pivarski <jpivarski@gmail.com>
2016-02-18 12:19:20 -0600

    Fresh start (everything before this is in branch 'FreeHep-with-hadoop-nospark')

---
+++ src/main/cpp/root2avro.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <avro.h>
+
+int main(int argc, char **argv) {
+  printf("BEGIN\n");
+
+
+  printf("END\n");
+}
--- src/main/scala/org/dianahep/scaroot/Main.scala
+++ src/main/scala/org/dianahep/scaroot/Main.scala
@@ -1,7 +1,5 @@
 package org.dianahep
 
-import com.sun.jna._
-
 package scaroot {
   object Main {
     def main(args: Array[String]) {}

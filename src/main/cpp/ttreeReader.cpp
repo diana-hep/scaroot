560ffbdbc30470c047b361620ad5fe4396a9d6e5
Jim Pivarski <jpivarski@gmail.com>
2016-02-19 10:47:23 -0600

    Move things around again to accomodate BridJ.

---
+++ src/main/cpp/ttreeReader.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <TROOT.h>
+#include <TInterpreter.h>
+#include <TFile.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+// int main(int argc, char **argv) {
+//   // std::cout << "BEGIN" << std::endl;
+
+//   auto code = std::string("#include \"Event.h\"\n") +
+//     std::string("void run() {\n") +
+//     std::string("  TFile fileIn(\"/home/pivarski/tmp/freehep_tests/Event.root\");\n") +
+//     std::string("  TTreeReader theReader(\"T\", &fileIn);\n") +
+//     std::string("  TTreeReaderValue<Event> eventRV(theReader, \"event\");\n") +
+//     std::string("  TTreeReaderValue<Int_t> nTracksRV(theReader, \"fNtrack\");\n") +
+//     std::string("  while (theReader.Next()) {\n") +
+//     std::string("    auto event = eventRV.Get();\n") +
+//     std::string("    std::cout << *nTracksRV << \" \" << event->GetNseg() << \" \" << event->GetTemperature() << std::endl;\n") +
+//     std::string("  }\n") +
+//     std::string("}\n");
+
+//   gROOT->ProcessLine(".L Event_cxx.so");
+
+//   TInterpreter *tInterpreter = gROOT->GetInterpreter();
+//   tInterpreter->Declare(code.c_str());
+
+//   gROOT->ProcessLine("run();");
+
+//   // std::cout << "END" << std::endl;
+// }

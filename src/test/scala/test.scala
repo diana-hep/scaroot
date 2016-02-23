8e6ace46f1446065a1452a9e45c7ed8c6c6f54ab
Jim Pivarski <jpivarski@gmail.com>
2016-02-22 21:15:04 -0600

    Seemed for a long time to be segfault-free, and then...

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -7,29 +7,36 @@ import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.Matchers
 
+import com.sun.jna.Native
+
 import org.dianahep.scaroot._
 
 class DefaultSuite extends FlatSpec with Matchers {
   "JNA" must "work" in {
+    Native.setProtected(true)
+    println("Native.isProtected", Native.isProtected)
+
+    val lib = RootTreeReaderLibrary.INSTANCE
+
     println("one")
-    val file = RootTreeReaderLibrary.INSTANCE.newFile("/home/pivarski/fun/rio/TrackResonanceNtuple.root")
+    val file = lib.newFile("/home/pivarski/fun/rio/TrackResonanceNtuple.root")
     println("two")
-    val reader = RootTreeReaderLibrary.INSTANCE.newReader(file, "TrackResonanceNtuple/twoMuon")
+    val reader = lib.newReader(file, "TrackResonanceNtuple/twoMuon")
     println("three")
-    val mass = RootTreeReaderLibrary.INSTANCE.newValue_float(reader, "mass_mumu")
+    val mass = lib.newValue_float(reader, "mass_mumu")
     println("four")
     var counter = 0
-    while (RootTreeReaderLibrary.INSTANCE.readerNext(reader) > 0) {
+    while (lib.readerNext(reader) > 0) {
       // println("five")
-      // println(RootTreeReaderLibrary.INSTANCE.getValue_float(mass))
-      val num = RootTreeReaderLibrary.INSTANCE.getValue_float(mass)
+      // println(lib.getValue_float(mass))
+      val num = lib.getValue_float(mass)
       println(num)
       counter += 1
-      if (counter % 1000 == 0) {
-        println("gc start")
-        System.gc()
-        println("gc end")
-      }
+      // if (counter % 1000 == 0) {
+      //   println("gc start")
+      //   System.gc()
+      //   println("gc end")
+      // }
     }
     println("six")
   }

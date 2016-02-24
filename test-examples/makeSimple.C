3e510458b2e64b42db391323bcce7d00236fcb21
Jim Pivarski <jpivarski@gmail.com>
2016-02-24 13:11:19 -0600

    try to make more test examples, thwarted by missing class data in TBranchElement

--- test-examples/makeSimple.C
+++ test-examples/makeSimple.C
@@ -1,6 +1,9 @@
+#include <string>
+
 #include "Riostream.h"
 
 void makeSimple() {
+
   TFile *f = new TFile("verysimple.root", "RECREATE");
   TNtuple *ntuple = new TNtuple("ntuple", "fake data", "x:y:z");
 
@@ -18,7 +21,8 @@ void makeSimple() {
   Int_t one;
   Float_t two;
   Char_t three[10];
-  TString four;
+  TString *fourfour = new TString;
+  std::string four;
   Float_t five[2];
   Float_t six[4];
   Float_t seven[2][2];
@@ -27,16 +31,21 @@ void makeSimple() {
   tree->Branch("one", &one, "one/I");
   tree->Branch("two", &two, "two/F");
   tree->Branch("three", &three, "three/C");
-  tree->Branch("four", &four, 16000, 0);
+  tree->Branch("fourfour", "TString", &fourfour);
+  tree->Branch("four", &four);
   tree->Branch("five", &five, "five[2]/F");
   tree->Branch("six", &six, "six[one]/F");
   tree->Branch("seven", &seven, "seven[2][2]/F");
-  tree->Branch("eight", &eight, 16000, 0);
+  TBranch *check = tree->Branch("eight", &eight, 16000, 0);
+
+  std::cout << "check " << (((TBranchElement*)check)->GetType()) << " " << TBranchElement::kSTLNode << std::endl;
 
   one = 1;
   two = 1.1;
   strcpy(three, std::string("uno").c_str());
-  four = "uno";
+  fourfour->Clear();
+  fourfour->Append("uno");
+  four.assign("uno");
   five[0] = 3.14;
   five[1] = 6.28;
   six[0] = 1.1;
@@ -44,13 +53,15 @@ void makeSimple() {
   seven[0][1] = 0.0;
   seven[1][0] = 0.0;
   seven[1][1] = 1.1;
-  eight.Add(new TObjString(four));
+  eight.Add(new TObjString("uno"));
   tree->Fill();
 
   one = 2;
   two = 2.2;
   strcpy(three, std::string("dos").c_str());
-  four = "dos";
+  fourfour->Clear();
+  fourfour->Append("dos");
+  four.assign("dos");
   five[0] = -3.14;
   five[1] = -6.28;
   six[0] = 1.1;
@@ -59,13 +70,15 @@ void makeSimple() {
   seven[0][1] = 0.0;
   seven[1][0] = 0.0;
   seven[1][1] = 2.2;
-  eight.Add(new TObjString(four));
+  eight.Add(new TObjString("dos"));
   tree->Fill();
 
   one = 3;
   two = 3.3;
   strcpy(three, std::string("tres").c_str());
-  four = "tres";
+  fourfour->Clear();
+  fourfour->Append("tres");
+  four.assign("tres");
   five[0] = 1;
   five[1] = 2;
   six[0] = 1.1;
@@ -75,13 +88,15 @@ void makeSimple() {
   seven[0][1] = 0.0;
   seven[1][0] = 0.0;
   seven[1][1] = 3.3;
-  eight.Add(new TObjString(four));
+  eight.Add(new TObjString("tres"));
   tree->Fill();
 
   one = 4;
   two = 4.4;
   strcpy(three, std::string("quatro").c_str());
-  four = "quatro";
+  fourfour->Clear();
+  fourfour->Append("quatro");
+  four.assign("quatro");
   five[0] = -1;
   five[1] = -2;
   six[0] = 1.1;
@@ -92,7 +107,7 @@ void makeSimple() {
   seven[0][1] = 0.0;
   seven[1][0] = 0.0;
   seven[1][1] = 4.4;
-  eight.Add(new TObjString(four));
+  eight.Add(new TObjString("quatro"));
   tree->Fill();
 
   f2->Write();
@@ -101,22 +116,33 @@ void makeSimple() {
   // TFile *f2again = new TFile("simple.root");
   // TTree *treeagain;
   // f2again->GetObject("tree", treeagain);
-  // treeagain->Scan();
+  // treeagain->Scan("eight");
 
   TFile *f3 = new TFile("notsosimple.root", "RECREATE");
   TTree *tree2 = new TTree("tree", "fake data");
   Double_t scalar;
   std::vector<Double_t> vector;
+  std::vector<std::string> vector2;
   std::vector<std::vector<Double_t> > tensor;
+  // std::vector<std::vector<std::string> > tensor2;
   tree2->Branch("scalar", &scalar);
   tree2->Branch("vector", &vector);
+  tree2->Branch("vector2", &vector2);
   tree2->Branch("tensor", &tensor);
+  // tree2->Branch("tensor2", &tensor2);
 
   scalar = 3.14;
   vector = {1.1, 2.2, 3.3};
+  vector2 = {std::string("one"), std::string("two"), std::string("three")};
   tensor = {{0.9, 0.1}, {-0.1, 0.9}};
+  // tensor2 = {{std::string("a"), std::string("b")}, {std::string("c"), std::string("d")}};
   tree2->Fill();
 
   f3->Write();
   f3->Close();
+
+  // TFile *f3again = new TFile("notsosimple.root");
+  // TTree *treeagain;
+  // f3again->GetObject("tree", treeagain);
+  // treeagain->Scan();
 }

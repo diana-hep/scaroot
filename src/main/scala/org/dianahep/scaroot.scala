3266fa9fd83c887e2c0ea40d4788e16b7452de5d
Jim Pivarski <jpivarski@gmail.com>
2016-03-09 12:09:50 -0600

    fleshed out macro, start to integrate ROOT with macro

--- src/main/scala/org/dianahep/scaroot.scala
+++ src/main/scala/org/dianahep/scaroot.scala
@@ -4,21 +4,32 @@ import scala.language.experimental.macros
 // import scala.reflect.macros.blackbox.Context
 import scala.reflect.macros.Context
 
-// import com.sun.jna.Pointer
-// import com.sun.jna.ptr.PointerByReference
-// import com.sun.jna.Memory
-// import com.sun.jna.Native
+import com.sun.jna.Pointer
+import com.sun.jna.ptr.PointerByReference
+import com.sun.jna.Memory
+import com.sun.jna.Native
 
 package scaroot {
+  trait Param {
+    def name: String
+  }
+  case class IntParam(name: String) extends Param
+
+  trait Ret
+  case object IntRet extends Ret
+
+  case class Method(name: String, params: List[Param], ret: Ret)
+
   trait RootClassFactory[INTERFACE] {
     def className: String
+    def methods: List[Method]
     def cpp: String
     def newInstance: INTERFACE
   }
 }
 
 package object scaroot {
-  // RootAccessLibrary.resetSignals()
+  RootAccessLibrary.resetSignals()
 
   def rootClassFactory[INTERFACE](cpp: String): RootClassFactory[INTERFACE] = macro rootClassFactoryImpl[INTERFACE]
 
@@ -28,12 +39,37 @@ package object scaroot {
 
     val name = interface.toString.split('.').last
 
+    val undefined = interface.declarations.
+      filter(_.asInstanceOf[scala.reflect.internal.Symbols#Symbol].hasFlag(scala.reflect.internal.Flags.DEFERRED))
+        
+    val (methodsReflect, methodsReal) = undefined.toList.collect {case method: MethodSymbol =>
+      val (paramsReflect, paramsReal) = method.paramss.flatten.map {p =>
+        if (p.typeSignature =:= typeOf[Int])
+          (q"IntParam(${p.name.toString})", q"${stringToTermName(p.name.toString)}: ${p.typeSignature}")
+        else
+          throw new Exception(p.typeSignature.toString)
+      }.unzip
+
+      val (retReflect, retReal) =
+        if (method.returnType =:= typeOf[Int])
+          (q"IntRet", method.returnType)
+        else
+          throw new Exception("oops 2")
+
+      (q"Method(${method.name.toString}, List(..$paramsReflect), $retReflect)",
+        q"def ${stringToTermName(method.name.toString)}(..$paramsReal): $retReal = 999")
+    }.unzip
+
+    println(methodsReflect)
+    println(methodsReal)
+
     c.Expr[RootClassFactory[INTERFACE]](q"""
       new RootClassFactory[$interface] {
-        def className: String = $name
-        def cpp: String = $cpp
+        val className = $name
+        val methods = List(..$methodsReflect)
+        val cpp = $cpp
         def newInstance: $interface = new $interface {
-          def hello(x: String): String = "hey " + x
+          ..$methodsReal
         }
       }
     """)
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -7,69 +7,73 @@ import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.Matchers
 
-// import com.sun.jna.Pointer
-// import com.sun.jna.ptr.PointerByReference
-// import com.sun.jna.Memory
-// import com.sun.jna.Native
+import com.sun.jna.Pointer
+import com.sun.jna.ptr.PointerByReference
+import com.sun.jna.Memory
+import com.sun.jna.Native
 
 import org.dianahep.scaroot._
 
 class DefaultSuite extends FlatSpec with Matchers {
-//   "RootAccess" must "work" in {
-//     RootAccessLibrary.resetSignals()
+  "RootAccess" must "work" in {
+    RootAccessLibrary.resetSignals()
 
-//     RootAccessLibrary.declare("""
-// class Something {
-// public:
-//   int plus(int x, int y) { return x + y; }
-// };
-// """)
+    RootAccessLibrary.declare("""
+class Something {
+public:
+  int plus(int x, int y) { return x + y; }
+};
+""")
 
-//     val tclass = RootAccessLibrary.tclass("Something")
+    val tclass = RootAccessLibrary.tclass("Something")
 
-//     val instance = RootAccessLibrary.newInstance(tclass)
+    val instance = RootAccessLibrary.newInstance(tclass)
 
-//     val numMethods = RootAccessLibrary.numMethods(tclass)
+    val numMethods = RootAccessLibrary.numMethods(tclass)
 
-//     val tmethods = 0 until numMethods map {methodIndex => RootAccessLibrary.tmethod(tclass, methodIndex)}
+    val tmethods = 0 until numMethods map {methodIndex => RootAccessLibrary.tmethod(tclass, methodIndex)}
 
-//     tmethods foreach {tmethod =>
-//       // print(RootAccessLibrary.tmethodName(tmethod))
-//       // print(" ")
-//       // print(RootAccessLibrary.tmethodNumArgs(tmethod))
-//       // print(" ")
+    tmethods foreach {tmethod =>
+      // print(RootAccessLibrary.tmethodName(tmethod))
+      // print(" ")
+      // print(RootAccessLibrary.tmethodNumArgs(tmethod))
+      // print(" ")
 
-//       val args = 0 until RootAccessLibrary.tmethodNumArgs(tmethod) map {argIndex => RootAccessLibrary.tmethodArg(tmethod, argIndex)}
-//       // println(args.mkString(" "))
-//     }
-
-//     val plus = RootAccessLibrary.tmethod(tclass, 0)
-
-//     val arg0 = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
-//     val arg1 = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
-//     val ret = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
-
-//     arg0.setInt(0, 3)
-//     arg1.setInt(0, 7)
-//     ret.setInt(0, 999)
-
-//     RootAccessLibrary.execute2(plus, instance, arg0, arg1, ret)
-
-//   }
-
-  "Scala macros" must "work" in {
-    trait MyInterface {
-      def hello(x: String): String
+      val args = 0 until RootAccessLibrary.tmethodNumArgs(tmethod) map {argIndex => RootAccessLibrary.tmethodArg(tmethod, argIndex)}
+      // println(args.mkString(" "))
     }
 
-    val factory = rootClassFactory[MyInterface]("code")
-    println(factory.className)
-    println(factory.cpp)
+    val plus = RootAccessLibrary.tmethod(tclass, 0)
 
-    val instance = factory.newInstance
+    val arg0 = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
+    val arg1 = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
+    val ret = new Memory(Native.getNativeSize(java.lang.Integer.TYPE))
 
-    println(instance)
-    println(instance.hello("fred"))
+    arg0.setInt(0, 3)
+    arg1.setInt(0, 7)
+    ret.setInt(0, 999)
+
+    RootAccessLibrary.execute2(plus, instance, arg0, arg1, ret)
 
   }
+
+  // "Scala macros" must "work" in {
+  //   trait MyInterface {
+  //     def one: Int
+  //     def two(x: Int): Int
+  //     def three(x: Int, y: Int): Int
+  //   }
+
+  //   val factory = rootClassFactory[MyInterface]("code")
+  //   println(factory.className)
+  //   println(factory.cpp)
+
+  //   val instance = factory.newInstance
+
+  //   println(instance)
+  //   println(instance.one)
+  //   println(instance.two(2))
+  //   println(instance.three(3, 3))
+
+  // }
 }
